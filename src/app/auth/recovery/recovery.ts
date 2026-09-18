d8e6416abe0327b6951c85d6002efff9a4d77e01
Dhemizzn <dhemizsaanolbert@gmail.com>
2026-09-18 18:12:36 -0500

    Auth components

---
+++ src/app/auth/recovery/recovery.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-recovery',
+  styleUrl: './recovery.scss',
+  templateUrl: './recovery.html',
+})
+export class Recovery {}
