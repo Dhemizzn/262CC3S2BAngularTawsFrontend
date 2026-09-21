688acff6c9c3997d8acfde72f5397fb3f628d63a
Dhemizzn <dhemizsaanolbert@gmail.com>
2026-09-21 18:34:40 -0500

    Register,Recovery HTML and SCSS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
+import { Recovery } from './auth/recovery/recovery';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'login', component: Login },
+  { path: 'register', component:Register},
+  { path: 'recovery', component:Recovery}
 ];
 
--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterLink],
   selector: 'app-login',
   styleUrl: './login.scss',
   templateUrl: './login.html',
